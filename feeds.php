ce2a2ab6b073fa2fb864ad41ab39975ba70ed702
Arun Raj <Arunraj07@users.noreply.github.com>
2018-11-10 21:14:17 +0530

    Initial commit

---
+++ feeds.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="src\styles.css">
+    <title>Feeds</title>
+  </head>
+  <body>
+    <div class="body">
+      <div class="nav">
+        <button type="button" name="button" class="feedbutton">Feeds</button>
+        <button type="button" name="button" class="profilebutton">Profile</button>
+      </div>
+      <div class="feed">
+      </div>
+    </div>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="src\styles.css">
+    <title>Login</title>
+  </head>
+  <body>
+
+    <div class="presentoffm" id="topofpage">
+    <div align="center" style="margin:10px;display:block;">
+      <h1>2018 Office Bearers</h1>
+    </div>
+    <div class="majoroff">
+    <div class="cntctcard" align="center">
+        <img width="130px" height="130px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/IMG-20180210-WA0013.jpg" alt="Prof. Muhammad Kasim S">
+        <h4>Prof. Muhammad Kasim S</h4>
+        <h5>Chair</h5>
+        <p>Professor &amp; Head</p>
+        <p>Musaliar College of Engineering Chirayinkeezhu</p>
+          <p >Email: sm_kasim78@yahoo.com</p>
+          <p >Phone: 9895141715</p>
+    </div>
+
+    <div class="cntctcard" align="center">
+        <img width="130px" height="130px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/20141130_162140-150x129-e1518457259555.jpg" alt="Dr. K.Bijuna">
+        <h4>Dr. K.Bijuna</h4>
+        <h5>Vice Chair</h5>
+        <p>Professor, Dept. of Electrical & Electronics Engineering</p>
+        <p>TKM College of Engineering, Kollam</p>
+          <p >Email: kbijuna@gmail.com</p>
+          <p >Phone: 9895163961</p>
+    </div>
+
+    <div class="cntctcard" align="center">
+        <img width="130px" height="130px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/biju-photo-768x919-e1518457092462.jpg" alt="Mr. Biju K">
+        <h4>Mr. Biju K</h4>
+        <h5>Secretary</h5>
+        <p>Assistant Professor, Dept of Electrical Engineering</p>
+        <p>College of Engineering Munnar</p>
+          <p >Email: bijuk@ieee.org</p>
+          <p >Phone: 9446808243</p>
+    </div>
+
+    <div class="cntctcard" align="center">
+        <img width="130px" height="130px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/Rijil-Ramchand-Photo-e1518456412211.jpg" alt="Dr. Rijil Ramchand">
+        <h4>Dr. Rijil Ramchand</h4>
+        <h5>Immediate Past Chair</h5>
+        <p>Associate professor, Department of Electrical Engineering</p>
+        <p>NIT Calicut</p>
+          <p >Email: -</p>
+          <p >Phone: -</p>
+    </div>
+
+    </div>
+    <div class="minoroff">
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/IMG-20180210-WA0023.jpg" alt="Dr. Jaison Mathew">
+      <h4>Dr. Jaison Mathew</h4>
+      <h5>Chair-Conference Activities</h5>
+      <p>Associate professor</p>
+      <p>GEC Thrissur</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/WhatsApp-Image-2018-02-12-at-1.58.07-PM-e1518449644426.jpeg" alt="Mr. Srikanth vasudevan">
+      <h4>Mr. Srikanth Vasudevan</h4>
+      <h5>Area Chair</h5>
+      <p>Asst Professor (Sr.Gd) Dept of EEE</p>
+      <p>Amrita Vishwa Vidyapeetham</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/WhatsApp-Image-2018-02-12-at-9.06.07-AM.jpeg" alt="Er. C M Varghese">
+      <h4>Er. C M Varghese</h4>
+      <h5>Chair-Industry Relations</h5>
+      <p>Director</p>
+      <p>Igatech industries ltd Kochi</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/subash-e1518456685686.jpg" alt="Mr. Subhash Joshi">
+      <h4>Mr. Subhash Joshi</h4>
+      <h5>Chair-Research</h5>
+      <p>Senior Engineer</p>
+      <p>CDAC Trivandrum</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/images-e1518455890561.jpg" alt="Dr. Sunil T T">
+      <h4>Dr. Sunil T T</h4>
+      <h5>Chair-Projects</h5>
+      <p>Principal</p>
+      <p>College of Engineering Attingal</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/Rajeevan-PP.jpg" alt="Dr. Rajeevan P P">
+      <h4>Dr. Rajeevan P P</h4>
+      <h5>Chair-Industry-Institute Interactions</h5>
+      <p>Associate professor</p>
+      <p>IIST Trivandrum</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/429de834-6379-4862-87ae-99627eda7885-e1518450754946.jpg" alt="Mr. Asok Kumar">
+      <h4>Mr. Asok Kumar</h4>
+      <h5>Chair-Student Branch Chapter Coordinations</h5>
+      <p>Assistant professor</p>
+      <p>CET</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/333949b2-f9b2-40c0-8b5d-8595d2bb9693-e1518456060597.jpg" alt="Dr. Pinkymol K P">
+      <h4>Dr. Pinkymol K P</h4>
+      <h5>Chair-Membership Development</h5>
+      <p>Associate professor</p>
+      <p>St Gits College of Engineering Kottayam</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/WhatsApp-Image-2018-02-12-at-7.30.45-PM-e1518456204454.jpeg" alt="Dr. Vasanthi V">
+      <h4>Dr. Vasanthi V</h4>
+      <h5>Representative-Malabar Subsection</h5>
+      <p>Associate professor</p>
+      <p>NSS College of Engineering Palakkad</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/IMG-20180221-WA0000-e1519153861196.jpg" alt="Mr. Jithin K Jose">
+      <h4>Mr. Jithin K Jose</h4>
+      <h5>Representative-Kochi Subsection</h5>
+      <p>Assistant Professor Dept. of EEE</p>
+      <p>Jyothi Engineering College Cheruthuruthy</p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/03/thomassabu-e1520853359326.jpeg" alt="Mr. Thomas Sabu">
+      <h4>Mr. Thomas Sabu</h4>
+      <h5>Representative-Young Professionals</h5>
+      <p></p>
+      <p></p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/03/jefythomas-e1520847610481.jpg" alt="Ms. Jefy Thomas">
+      <h4>Ms. Jefy Thomas</h4>
+      <h5>Representative-WIE</h5>
+      <p></p>
+      <p></p>
+        <p >Email: -</p>
+        <p >Phone: -</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/IMG-20180221-WA0001-e1519154206745.jpg" alt="Mr. Abhinand A S">
+      <h4>Mr. Abhinand A S</h4>
+      <h5>Student Representative</h5>
+      <p>Student ECE</p>
+      <p>TKMCE Kollam</p>
+        <p >Email: 8606385485</p>
+        <p >Phone: abhinand4as@ieee.org</p>
+    </div>
+
+    <div class="cntctcards" align="center">
+      <img width="100px" height="100px" src="https://ia.ie.pels.ieeekerala.org/wp-content/uploads/2018/02/me-e1518452217316.jpg" alt="Mr. Arun Raj A R">
+      <h4>Mr. Arun Raj A R</h4>
+      <h5>Webmaster</h5>
+      <p>Student CSE</p>
+      <p>GCE Kannur</p>
+        <p >Email: 9562798150</p>
+        <p >Phone: arunrajar@computer.org</p>
+      </div>
+    </div>
+      <a class="mvtotop" align="center" href="#topofpage" style="color:white; text-decoration:none;"><b>top</b></a>
+
+
+
+  </body>
+</html>
